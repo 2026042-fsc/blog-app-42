8851904a9d46796a1f1c0b235a8ac3255db0747b
ichida <=2026042@fsc-educ.com>
2026-06-05 14:36:54 +0900

    home画面全要素表示

--- src/main/java/com/example/blog_app/Blog.java
+++ src/main/java/com/example/blog_app/Blog.java
@@ -1,32 +1,38 @@
 package com.example.blog_app;
 
 public class Blog {
+    // private final int id;
     private final String title;
-    private final String text;
+    // private final String text;
     private final String image;
-    private final String auther;
+    private final String author;
+    // private final String created_at;
 
-    public Blog(String title, String text, String image, String auther) {
-        this.title = "";
-        this.text = "";
-        this.image = "";
-        this.auther = "";
+    public Blog(String title,String image, String author) {
+        this.title = title;
+        // this.text = text;
+        this.image = image;
+        this.author = author;
+        // this.created_at = created_at;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getText() {
-        return text;
-    }
+    // public String getText() {
+    //     return text;
+    // }
 
     public String getImage() {
         return image;
     }
 
-    public String getAuther() {
-        return auther;
+    public String getAuthor() {
+        return author;
     }
+    // public String getCreated_at() {
+    //     return created_at;
+    // }
 
 }
