5e4c69c1bff0115003bc4269e77c5f1e697c0ce5
ichida <=2026042@fsc-educ.com>
2026-06-10 17:24:39 +0900

    コントローラーコメント書いたよ

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -19,17 +19,21 @@ public class BlogController {
         this.blogRepository = blogRepository;
     }
 
+    //ホームへの遷移
     @GetMapping("/")
     public String home(Model model) {
+        //全件表示
         model.addAttribute("blogs", blogRepository.home());
         return "home";
     }
 
+    //サイトの説明
     @GetMapping("/about")
     public String about() {
         return "about";
     }
 
+    //詳細ページに飛ぶ
     @GetMapping("/blog/{id}")
     public String detail(@PathVariable int id, Model model) {
         Optional<Blog> BlogOpt = blogRepository.searchById(id);
@@ -40,26 +44,31 @@ public class BlogController {
         return "blog/detail";
     }
 
+    //更新
     @GetMapping("/create/createNewBlog")
     public String createNewBlog(Model model) {
         model.addAttribute("blog", new Blog(0, "", "", "", "", ""));
         return "/create/createNewBlog";
     }
 
+    //blogの保存
     @PostMapping("/create/save")
     public String saveBlog(@ModelAttribute Blog blog) {
         blogRepository.save(blog);
         return "create/complet";
     }
 
+    //blogの保存
     @GetMapping("create/complet")
     public String complet() {
         return "create/complet";
     }
 
+    //検索
     @GetMapping("/search")
+    //required = false　空白でもエラー出さないでねー
     public String blog(@RequestParam(required = false) String keyword, Model model) {
-  
+        //なんか書いてあるときは、表示してね。
         if (keyword != null && ! keyword.isBlank()) {
             List<Blog> blogs = blogRepository.searchByTitle(keyword);
             model.addAttribute("blogs",blogs);
